e0292d099e70f7c884030aadaae190976b86cf93
sunyamare <sunyamare@gmail.com>
2020-01-20 12:03:53 +0100

    installed uuid

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
@@ -58,17 +59,19 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <div className="container">
-                    <Header />
-                    <AddTodo addTodo={this.addTodo} />
-                    <Todos
-                        todos={this.state.todos}
-                        markComplete={this.markComplete}
-                        delTodo={this.delTodo}
-                    />
+            <Router>
+                <div className="App">
+                    <div className="container">
+                        <Header />
+                        <AddTodo addTodo={this.addTodo} />
+                        <Todos
+                            todos={this.state.todos}
+                            markComplete={this.markComplete}
+                            delTodo={this.delTodo}
+                        />
+                    </div>
                 </div>
-            </div>
+            </Router>
         );
     }
 }
